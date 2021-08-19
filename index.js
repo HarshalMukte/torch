b88bb548b2b22913020b31a1ab1cea6c6fb43f69
Harshal <harshalmukte@gmail.com>
2021-08-19 10:55:08 +0530

    improvements

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const btn = document.getElementById("btn");
+let btn = document.getElementById("btn");
 let light = document.querySelector(".light");
+let container = document.querySelector(".container");
+
 btn.onclick = () => {
     btn.classList.toggle("active");
     light.classList.toggle("active");
-    document.querySelector(".container").classList.toggle("active");
+    container.classList.toggle("active");
 }
